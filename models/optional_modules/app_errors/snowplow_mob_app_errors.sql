469aa6b3c65c3d80d7d797edbfde60d8c8508e9c
emielver <emiel@snowplowanalytics.com>
2021-12-16 12:41:28 +0100

    Corrected the reference to the upstream table

--- models/optional_modules/app_errors/snowplow_mob_app_errors.sql
+++ models/optional_modules/app_errors/snowplow_mob_app_errors.sql
@@ -16,5 +16,5 @@
 
 
 select * 
-from {{ ref('snowplow_mob_base_events_this_run') }}
+from {{ ref('snowplow_mob_app_errors_this_run') }}
 where {{ snowplow_utils.is_run_with_new_events('snowplow_mob') }} --returns false if run doesn't contain new events.
