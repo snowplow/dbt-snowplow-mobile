19d83f75a7ca07cb7de9a59c941a23f10e9bb177
Ryan Hill <r.l.hill128@gmail.com>
2023-02-21 10:42:10 +0000

    Move ref in enabled check (Close #61)

--- macros/stitch_user_identifiers.sql
+++ macros/stitch_user_identifiers.sql
@@ -1,20 +1,20 @@
-{% macro stitch_user_identifiers(enabled, relation=this, user_mapping_relation=ref('snowplow_mobile_user_mapping')) %}
+{% macro stitch_user_identifiers(enabled, relation=this, user_mapping_relation='snowplow_mobile_user_mapping') %}
 
   {% if enabled and target.type not in ['databricks', 'spark'] | as_bool() %}
 
     -- Update sessions table with mapping
     update {{ relation }} as s
     set stitched_user_id = um.user_id
-    from {{ user_mapping_relation }} as um
+    from {{ ref(user_mapping_relation) }} as um
     where s.device_user_id = um.device_user_id;
 
 {% elif enabled and target.type in ['databricks', 'spark']  | as_bool() %}
 
     -- Update sessions table with mapping
     merge into {{ relation }} as s
-    using {{ user_mapping_relation }} as um
+    using {{ ref(user_mapping_relation) }} as um
     on s.device_user_id = um.device_user_id
-    when matched then 
+    when matched then
       update set s.stitched_user_id = um.user_id;
 
   {% endif %}
