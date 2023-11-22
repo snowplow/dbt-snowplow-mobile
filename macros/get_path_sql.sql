7f57ab22657d2b3e600f6517bbc87fed8079bb7a
Emiel <emiel.verkade@gmail.com>
2023-11-22 11:04:37 +0000

    Update license

--- macros/get_path_sql.sql
+++ macros/get_path_sql.sql
@@ -1,3 +1,10 @@
+{#
+Copyright (c) 2021-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Community License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
+#}
+
 {% macro get_session_id_path_sql(relation_alias) %}
 
   {{ return(adapter.dispatch('get_session_id_path_sql', 'snowplow_mobile')(relation_alias)) }}
