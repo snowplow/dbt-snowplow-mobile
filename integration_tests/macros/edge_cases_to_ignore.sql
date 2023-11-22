1cfc4f3f6a20dfdb239bed1ed8b278ae654be04f
Emiel <emiel.verkade@gmail.com>
2023-11-22 11:04:37 +0000

    Update license

--- integration_tests/macros/edge_cases_to_ignore.sql
+++ integration_tests/macros/edge_cases_to_ignore.sql
@@ -1,3 +1,10 @@
+{#
+Copyright (c) 2021-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Community License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
+#}
+
 {# Filters out edge cases in test data set which we havent yet solved for - currently illustration only #}
 {% macro edge_cases_to_ignore() %}
   user_id not in (
